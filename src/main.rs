bbacb7f446f3cc68308f7567c089c35d9fc0d857
typable <typable@prototype-studio.de>
2021-08-23 10:57:38 +0200

    Added outfile command

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,16 @@ use std::fs;
 use std::io::Result;
 use std::path::PathBuf;
 
+#[cfg(windows)]
+const LINE_BREAK: &'static str = "\r\n";
+#[cfg(not(windows))]
+const LINE_BREAK: &'static str = "\n";
+
 fn main() {
     let mut path = vec!["."];
     let depth = vec![];
     let mut location = ".";
+    let mut file = "";
     let mut all = false;
     let mut level = usize::MAX;
     let mut args = env::args().collect::<Vec<String>>();
@@ -14,33 +20,38 @@ fn main() {
     for arg in &args {
         match arg {
             arg if arg.starts_with("-p") || arg.starts_with("--path") => {
-                let parts = arg.split("=")
-                    .filter(|&part| !part.is_empty())
-                    .collect::<Vec<&str>>();
-                if parts.len() == 2 {
-                    location = parts[1];
-                }
-                else {
-                    println!("Error: Invalid argument! Use: [--path|-p]=<location>");
-                    return;
+                match property(arg) {
+                    Some(value) => location = value,
+                    None => {
+                        println!("Error: Invalid argument! Use: [--path|-p]=<location>");
+                        return;
+                    },
                 }
             },
             arg if arg.starts_with("-l") || arg.starts_with("--level") => {
-                let parts = arg.split("=")
-                    .filter(|&part| !part.is_empty())
-                    .collect::<Vec<&str>>();
-                if parts.len() == 2 {
-                    match parts[1].to_string().parse::<usize>() {
-                        Ok(depth) => level = depth,
-                        Err(_) => {
-                            println!("Error: Invalid argument! Use: [--level|-l]=<depth>");
-                            return;
-                        },
-                    }
+                match property(arg) {
+                    Some(value) => {
+                        match value.to_string().parse::<usize>() {
+                            Ok(depth) => level = depth,
+                            Err(_) => {
+                                println!("Error: Invalid argument! Use: [--level|-l]=<depth>");
+                                return;
+                            },
+                        }
+                    },
+                    None => {
+                        println!("Error: Invalid argument! Use: [--level|-l]=<depth>");
+                        return;
+                    },
                 }
-                else {
-                    println!("Error: Invalid argument! Use: [--level|-l]=<depth>");
-                    return;
+            },
+            arg if arg.starts_with("-o") || arg.starts_with("--out") => {
+                match property(arg) {
+                    Some(value) => file = value,
+                    None => {
+                        println!("Error: Invalid argument! Use: [--out|-o]=<file>");
+                        return;
+                    },
                 }
             },
             arg if arg.starts_with("-a") || arg.starts_with("--all") => {
@@ -60,14 +71,28 @@ fn main() {
             .map(|(_, part)| part)
             .collect::<Vec<&str>>();
     }
-    println!("{}/", path.join("/"));
+    let mut tree = vec![format!("{}/", path.join("/"))];
     match walk(path, depth, level, all) {
-        Ok(_) => (),
+        Ok(result) => {
+            let (_, mut sub_tree) = result;
+            tree.append(&mut sub_tree);
+            let output = tree.join(LINE_BREAK);
+            match file.len() > 0 {
+                true => {
+                    match fs::write(file, output) {
+                        Ok(_) => println!("Written into output file."),
+                        Err(_) => println!("Unable to resolve location of output file!"),
+                    }
+                },
+                _ => println!("{}", output),
+            }
+        },
         Err(_) => println!("Error: Unable to resolve location!"),
     }
 }
 
-fn walk(path: Vec<&str>, mut depth: Vec<bool>, level: usize, all: bool) -> Result<Vec<bool>> {
+fn walk(path: Vec<&str>, mut depth: Vec<bool>, level: usize, all: bool) -> Result<(Vec<bool>, Vec<String>)> {
+    let mut result = Vec::new();
     let tree = indent(&depth);
     let mut alive = true;
     let entries = fs::read_dir(path.join("/"))?
@@ -86,21 +111,33 @@ fn walk(path: Vec<&str>, mut depth: Vec<bool>, level: usize, all: bool) -> Resul
         if all || !file.starts_with(".") {
             let meta = fs::metadata(format!("{}/{}", path.join("/"), file))?;
             if meta.is_dir() {
-                println!("{}{}{}/", tree, symbol, file);
+                result.push(format!("{}{}{}/", tree, symbol, file));
                 if depth.len() < level {
                     let mut branch = path.clone();
                     branch.push(&file);
                     depth.push(alive);
-                    depth = walk(branch, depth, level, all)?;
+                    let (sub_depth, mut sub_tree) = walk(branch, depth, level, all)?;
+                    depth = sub_depth;
+                    result.append(&mut sub_tree);
                 }
             }
             else {
-                println!("{}{}{}", tree, symbol, file);
+                result.push(format!("{}{}{}", tree, symbol, file));
             }
         }
     }
     depth.pop();
-    Ok(depth)
+    Ok((depth, result))
+}
+
+fn property(arg: &str) -> Option<&str> {
+    let parts = arg.split("=")
+        .filter(|&part| !part.is_empty())
+        .collect::<Vec<&str>>();
+    match parts.len() == 2 {
+        true => Some(parts[1]),
+        _ => None
+    }
 }
 
 fn filename(entry: &PathBuf) -> String {
